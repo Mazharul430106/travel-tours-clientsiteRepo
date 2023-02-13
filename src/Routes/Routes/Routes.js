b5c0a2e31ffbe73b1c60e5370ab5c40f92869ab2
Mazharul Islam <mazharul430@gmail.com>
2023-02-13 03:12:10 -0800

    starting traveling tour site

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import About from "../../Pages/About/About";
+import Home from "../../Pages/Home/Home";
+
+export const routes = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/about',
+                element: <About></About>
+            },
+           
+        ]
+    }
+])
